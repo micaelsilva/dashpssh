f884474137f9285d5ba45603540d87abb967d224
Micael Silva <micaelsilva@gmail.com>
2024-04-24 13:10:36 -0300

    Create toml and update http client

--- src/pssh/__init__.py
+++ src/pssh/__init__.py
@@ -7,6 +7,7 @@ from pssh.httpclient import DefaultHTTPClient
 
 def load(
 	    uri,
+	    firstsegment=False,
 	    timeout=None,
 	    headers={},
 	    http_client=DefaultHTTPClient(),
@@ -14,7 +15,7 @@ def load(
 	):
     if urlsplit(uri).scheme:
         content, base_uri = http_client.download(uri, timeout, headers, verify_ssl)
-        return pssh.parse.parse(content, base_uri=base_uri)
+        return pssh.parse.parse(content, base_uri=base_uri, firstsegment=firstsegment)
     else:
     	pass
         #return _load_from_file(uri)
--- src/pssh/httpclient.py
+++ src/pssh/httpclient.py
@@ -11,14 +11,14 @@ class DefaultHTTPClient:
     def __init__(self, proxies=None):
         self.proxies = proxies
 
-    def download(self, uri, timeout=None, headers={}, verify_ssl=True):
+    def download(self, uri, timeout=None, headers={}, verify_ssl=True, binary=False):
         proxy_handler = urllib.request.ProxyHandler(self.proxies)
         https_handler = HTTPSHandler(verify_ssl=verify_ssl)
         opener = urllib.request.build_opener(proxy_handler, https_handler)
         opener.addheaders = headers.items()
         resource = opener.open(uri, timeout=timeout)
         base_uri = urljoin(resource.geturl(), ".")
-        content = resource.read().decode(
+        content = resource.read() if binary else resource.read().decode(
             resource.headers.get_content_charset(failobj="utf-8")
         )
         return content, base_uri
