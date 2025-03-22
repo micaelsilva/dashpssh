0c82641bdc0cb9aa2485e14e9f1be43be82573a4
Micael Silva <374220+micaelsilva@users.noreply.github.com>
2025-03-22 09:36:41 -0300

    Point test file to relative paths manifests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
+import os
 import logging
 import unittest
 
 import dashpssh
 
 log = logging.getLogger(__name__)
+root = os.path.dirname(os.path.realpath(__file__))
 
 
 class PsshTests(unittest.TestCase):
     def test_fixed_on_manifest(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/on_manifest/adaptationset.mpd"),
+                root + "/manifests/on_manifest/adaptationset.mpd"),
                 {'AAAAZnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAEYIARIQo2hWCauiSGufnJRmvwtINxoKdGVsZWZvbmljYSIkYTM2ODU2MDktYWJhMi00ODZiLTlmOWMtOTQ2NmJmMGI0ODM3'})
 
     def test_timeline_in_rep(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/timeline_in_rep/manifest.mpd"),
+                root + "/manifests/timeline_in_rep/manifest.mpd"),
                 {'AAAAP3Bzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB8IARIQgpmI8tbtQ0uk2VRj1CbnnSIJTGl2ZV8wMTY0'})
 
     def test_fixed_manifest_no_mimetype(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/on_manifest/no_mimetype.mpd"),
+                root + "/manifests/on_manifest/no_mimetype.mpd"),
                 {'AAAAbHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAEwSEKmx1TapT04wqABVkSnN4iAaDE1lZGlhc3RyZWFtMiIkYTliMWQ1MzYtYTk0Zi00ZTMwLWE4MDAtNTU5MTI5Y2RlMjIwSOPclZsG'})
 
     def test_fixed_on_manifest_remote(self):
@@ -36,7 +38,7 @@ class PsshTests(unittest.TestCase):
     def test_init(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/fixed/manifest.mpd"),
+                root + "/manifests/fixed/manifest.mpd"),
                 {'AAAAcXBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAFEIARIQPOwbHql5ndpVluZPN+XtIBoVdmVyaW1hdHJpeGNhYmxldmlzaW9uIh5yPXZteF93aWRld2luZV9UZWxlZmVIRFImcz05NTAqAlNEOAA='})
 
     @unittest.skip("temporarily without pssh")
@@ -51,14 +53,14 @@ class PsshTests(unittest.TestCase):
     def test_rotation_init_local(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/rotation/manifest.mpd",
+                root + "/manifests/rotation/manifest.mpd",
                 psshtype=dashpssh.PsshType.INIT),
                 {'AAAAaHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAEgIARIQU1yLEzKhgN5s2uOQeT3X+hoPdmVyaW1hdHJpeGNsYXJvIhpyPVNQT0hCU0hEX2Rhc2hfY2Umcz0yMzIwNioFU0RfSEQ='})
 
     def test_rotation_segment(self):
         self.assertEqual(
             dashpssh.load(
-                "tests/manifests/rotation/manifest.mpd",
+                root + "/manifests/rotation/manifest.mpd",
                 psshtype=dashpssh.PsshType.FIRSTSEGMENT),
                 {'AAAAaHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAEgIARIQBoqmCirRBx8WmAxl+p9SqxoPdmVyaW1hdHJpeGNsYXJvIhpyPVNQT0hCU0hEX2Rhc2hfY2Umcz0yMzIwNioFU0RfSEQ='})
 
